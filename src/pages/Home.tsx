28bd4a48ef5231a7ea6873e72f432fdd193294e8
Andres Quiroz <andresfqperez@gmail.com>
2026-01-21 18:16:49 -0500

    Explicacion y ejemplo de useEffect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ export const Home = () => {
       className="flex min-h-screen flex-col items-center
     justify-center p-4 bg-black text-white"
     >
-      <div className="bg-amber-400 mx-auto w-full max-w-3xl">
+      <div className="mx-auto w-full max-w-3xl">
         <h1
           className="text-3xl font-bold text-center mb-8
         "
--- src/pages/UseEffectPage.tsx
+++ src/pages/UseEffectPage.tsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from "react";
 import { BtnVolver } from "../components/ui/BtnVolver";
 
 export const UseEffectPage = () => {
+  const [segundos, setSegundos] = useState(0);
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      setSegundos((s) => s + 1);
+    }, 1000);
+    return () => clearInterval(intervalo);
+  }, []);
   return (
     <main className="bg-amber-500 h-screen">
       <BtnVolver />
-      <span>UseEffect</span>
+      <span>UseEffect: </span>
+      {segundos}
     </main>
   );
 };
